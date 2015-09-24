b8822be5cab3d685b33be5168eaeb9c64dcdd62f
Chad Jewsbury <cjewsbury@groupon.com>
2015-09-23 23:14:58 -0700

    Added Settings View and edited styles

--- tipcalculator/TipViewController.m
+++ tipcalculator/TipViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TipViewController.h"
+#import "SettingsViewController.h"
 
 @interface TipViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
@@ -23,6 +24,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Tip Calculator";
+    
+    // Set up the Settings button
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Defaults" style:UIBarButtonItemStylePlain target:self action:@selector(onSettingsButton)];
     [self updateValues];
 }
 
@@ -46,15 +50,22 @@
     [self updateValues];
 }
 
+- (void)onSettingsButton {
+    SettingsViewController *svc = [[SettingsViewController alloc] init];
+    svc.edgesForExtendedLayout = UIRectEdgeRight;
+    
+    [self.navigationController pushViewController:svc animated:YES];
+}
+
 - (void)updateValues {
     float billAmount = [self.billTextField.text floatValue];
     
-    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2), @(0.25)];
     float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
     
     float totalAmount = billAmount + tipAmount;
     
-    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.tipLabel.text = [NSString stringWithFormat:@"+ $%0.2f", tipAmount];
     self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
 }
 @end
